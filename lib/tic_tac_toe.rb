172a3f50e1b979d2282ed855c03364f1aaa01c85
Matthew Blank <msb277@cornell.edu>
2020-08-10 21:59:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,30 +19,28 @@ def display_board(board)
 end
 
 def input_to_index(input)
-    # take input, convert to integer and subtract 1
+  # take input, convert to integer and subtract 1
   i = input.to_i
   index = i - 1
 end
 
 def move(board, index, token)
+  # index in the board array that the player would like to fill out with an "X" or an "O"
+  # the player's character (either "X" or "O")
   m = board
   m[index] = token
   m
-
-  # index in the board array that the player would like to fill out with an "X" or an "O"
-  # the player's character (either "X" or "O")
-
 end
 
 def position_taken?(board, index)
-  board[index] != " " && !board[index].nil?
   # method will check to see if that board index is vacant or if it contains an "X" or an "O"
   # If the position is free, the method should return false (i.e. "not taken")
+  board[index] != " " && !board[index].nil?
 end
 
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
   # returns true if the move is valid and false or nil if not
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn(board)
@@ -59,22 +57,23 @@ def turn(board)
 end
 
 def turn_count(board)
+  # returns the number of turns that have been played.
   x = []
   board.select do |c|
     c != " " && !c.nil? ? x << c : a=a
   end
   x.length()
-  # returns the number of turns that have been played.
 end
 
 def current_player(board)
+  # use the #turn_count method to determine if it is "X"'s turn or "O"'s
   c = turn_count(board) % 2
   if c == 0
     current_player = "X"
   else
     current_player = "O"
   end
-  # use the #turn_count method to determine if it is "X"'s turn or "O"'s
+
 end
 
 def won?(b)
@@ -85,23 +84,23 @@ def won?(b)
 end
 
 def full?(board)
-  board.none?{|p| p == " "}
   # should accept a board and return true if every element in the board contains either an "X" or an "O".
   # return false if there is an available position and true if there is not
+  board.none?{|p| p == " "}
 end
 
 def draw?(board)
-  full?(board) && !(won?(board))
   # returns true if the board has not been won but is full
   # false if the board is not won and the board is not ful
   # false if the board is won
   # compose this method solely using the methods you used above with some ruby logi
+  full?(board) && !(won?(board))
 end
 
 def over?(board)
-  full?(board) || draw?(board) || won?(board)
   # returns true if the board has been won, is a draw, or is full
   # compose this method solely using the methods you used above with some ruby logic
+  full?(board) || draw?(board) || won?(board)
 end
 
 def winner(board)
