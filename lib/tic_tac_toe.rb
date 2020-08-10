01d588aee88cf10d9c8bf982ba766e23d3057229
Matthew Blank <msb277@cornell.edu>
2020-08-10 22:16:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -112,7 +112,7 @@ def winner(board)
   end
 end
 
-def play()
+def play(board)
   # the main method of the tic tac toe application and is responsible for the game loop
   # must allow players to take turns
   # checking if the game is over after every turn
