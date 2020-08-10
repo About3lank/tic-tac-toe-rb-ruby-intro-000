6c08f8d81ccbcc86fb4b31d7f0a46fde6b54dfda
Matthew Blank <msb277@cornell.edu>
2020-08-10 21:35:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,7 +46,7 @@ end
 def turn_count(board)
   x = []
   board.select do |c|
-    c != " " && !c.nil? ? x << c
+    c != " " && !c.nil? ? x << c :
   end
   x.length()
   # returns the number of turns that have been played.
