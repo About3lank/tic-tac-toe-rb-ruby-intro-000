451e4de86dc8fb0f4066bc69005298bdee64c7cf
Matthew Blank <msb277@cornell.edu>
2020-08-10 21:06:22 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,26 @@
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def display_board(board)
+  x = *(0..8)
+  puts" #{board[x.shift]} | #{board[x.shift]} | #{board[x.shift]} "
+  puts "-----------"
+  puts" #{board[x.shift]} | #{board[x.shift]} | #{board[x.shift]} "
+  puts "-----------"
+  puts" #{board[x.shift]} | #{board[x.shift]} | #{board[x.shift]} "
+end
+
+def input_to_index(input)
+  n = gets "Please enter a number from 1-9"
+  i = n - 1
+end
+
+puts input_to_index(5)
